27bb0d88480c2671f4ecb4aad6fd5594c85b2929
Trygve-F <trygvefinley@gmail.com>
2022-02-03 18:33:03 -0800

    added createUserGrid() and removeAllChildNodes()
    resize grid now working in constant dimension box

--- javascript.js
+++ javascript.js
@@ -1,10 +1,41 @@
-function makeDivs() {
+const grid = document.querySelector('.grid');
+
+function createGrid() {
 for (i = 0; i < 256; i++) {
     const div = document.createElement('div');
-    div.addEventListener('mouseenter', function(e) {
-        e.target.style.backgroundColor = 'aqua'
+    div.classList.add('cell')
+    div.addEventListener('click', function(e) {
+        e.target.style.backgroundColor = 'black'
         });
-    flexgrid.appendChild(div);
+    grid.appendChild(div);
+    }
+};
+
+function removeAllChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild)
     }
 }
-makeDivs()
+
+function createUserGrid() {
+    let val = prompt('Enter size of grid (eg. "10" is 10x10)');
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (let i = 0; i < val*val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('click', function(e) {
+            e.target.style.backgroundColor = 'black'
+    });
+    grid.appendChild(div)
+}
+}
+
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', function() {
+    removeAllChildNodes(grid);
+    createUserGrid();
+    })
+    
+
+createGrid()
