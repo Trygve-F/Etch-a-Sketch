c301ea99ac5b05546bf0ad81f0b0d7fe38c4d6de
Trygve-F <trygvefinley@gmail.com>
2022-02-04 11:27:43 -0800

    added index.html

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ function createGrid() {
 for (i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('cell')
-    div.addEventListener('click', function(e) {
+    div.addEventListener('mouseenter', function(e) {
         e.target.style.backgroundColor = 'black'
         });
     grid.appendChild(div);
@@ -15,7 +15,7 @@ function removeAllChildNodes(parent) {
     while(parent.firstChild) {
         parent.removeChild(parent.firstChild)
     }
-}
+};
 
 function createUserGrid() {
     let val = prompt('Enter size of grid (eg. "10" is 10x10)');
@@ -23,13 +23,12 @@ function createUserGrid() {
     for (let i = 0; i < val*val; i++) {
         const div = document.createElement('div');
         div.classList.add('cell');
-        div.addEventListener('click', function(e) {
+        div.addEventListener('mouseenter', function(e) {
             e.target.style.backgroundColor = 'black'
     });
     grid.appendChild(div)
+};
 }
-}
-
 
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', function() {
@@ -37,5 +36,10 @@ reset.addEventListener('click', function() {
     createUserGrid();
     })
     
+    const chooseColor = document.querySelector('#color');
+    chooseColor.addEventListener('input', function(){
+    let newColor = document.getElementById('color').value;
+    return newColor;
+    })
 
 createGrid()
